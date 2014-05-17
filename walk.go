62488177386c7da86119207ab5f540dac78c6688
bobobo1618 <bobobo1618@gmail.com>
2014-05-18 01:40:27 +1000

    Switched to absolute paths.

--- walk.go
+++ walk.go
@@ -3,13 +3,19 @@ package main
 import (
     "path/filepath"
     "os"
+    "log"
 )
 
 func walk(scanPaths []string, hashChan chan <- string) {
     for _, filePath := range scanPaths {
         filepath.Walk(filePath, func(filePath string, info os.FileInfo, err error) error {
             if !info.IsDir() {
-                hashChan <- filePath
+                absPath, err := filepath.Abs(filePath)
+                if err != nil {
+                    log.Printf("Error getting absolute path for %s - %s\n", filePath, err)
+                    return nil
+                }
+                hashChan <- absPath
             }
             return nil
         })
