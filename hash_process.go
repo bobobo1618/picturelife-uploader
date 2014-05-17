91bea25781c499836dca6c02b86f074579794dac
bobobo1618 <bobobo1618@gmail.com>
2014-05-18 02:27:10 +1000

    Parallelize hashing.

--- hash_process.go
+++ hash_process.go
@@ -6,64 +6,74 @@ import (
     "log"
     "io/ioutil"
     "fmt"
+    "sync"
 )
 
 func processHashChan(cache_dir string, hashChan chan string, sigCheckChan chan <- uploadJob){
+    var wg sync.WaitGroup
     for filePath := range hashChan {
+        wg.Add(1)
+        go func(filePath string){
+            defer wg.Done()
+            log.Printf("Processing %s\n", filePath)
+            // Look at the cache
+            cachePath := path.Join(cache_dir, filePath)
 
-        // Look at the cache
-        cachePath := path.Join(cache_dir, filePath)
+            var baseSum string;
 
-        var baseSum string;
-
-        fileInfo, err := os.Stat(filePath)
-        if err != nil {
-            log.Printf("Failed to get information about %s - %s\n", filePath, err)
-        }
-
-        lastModTime := fileInfo.ModTime().Unix()
-
-        // Check the cache
-        cacheFile, err := os.Open(cachePath)
-
-        if err != nil {
-            // If the cache file doesn't exist, create it.
-            if os.IsNotExist(err) {
-                baseSum, err = hashAndCache(cachePath, filePath, lastModTime)
-                if err != nil {
-                    log.Printf("Error hashing and caching: %s - %s", filePath, err)
-                }
-            } else {
-                log.Printf("Had an issue with the cache file: %s - %s\n", cachePath, err)
-                continue
+            fileInfo, err := os.Stat(filePath)
+            if err != nil {
+                log.Printf("Failed to get information about %s - %s\n", filePath, err)
+                return
             }
-        } else {
-            defer cacheFile.Close()
-            // Read the cache.
-            readBytes, err := ioutil.ReadAll(cacheFile)
+
+            lastModTime := fileInfo.ModTime().Unix()
+
+            // Check the cache
+            cacheFile, err := os.Open(cachePath)
 
             if err != nil {
-                log.Printf("Failed to read contents of cache file: %s - %s\n", cachePath, err)
-                continue
-            }
+                // If the cache file doesn't exist, create it.
+                if os.IsNotExist(err) {
+                    baseSum, err = hashAndCache(cachePath, filePath, lastModTime)
+                    if err != nil {
+                        log.Printf("Error hashing and caching: %s - %s", filePath, err)
+                        return
+                    }
+                } else {
+                    log.Printf("Had an issue with the cache file: %s - %s\n", cachePath, err)
+                    return
+                }
+            } else {
+                defer cacheFile.Close()
+                // Read the cache.
+                readBytes, err := ioutil.ReadAll(cacheFile)
 
-            readSum := string(readBytes)
-            var readModTime int64
-            fmt.Sscanf(readSum, "%d,%s", &readModTime, &baseSum)
-
-            // If the file has changed, recache it.
-            if readModTime != lastModTime {
-                baseSum, err = hashAndCache(cachePath, filePath, lastModTime)
                 if err != nil {
-                    log.Printf("Error hashing and caching: %s - %s", filePath, err)
+                    log.Printf("Failed to read contents of cache file: %s - %s\n", cachePath, err)
+                    return
+                }
+
+                readSum := string(readBytes)
+                var readModTime int64
+                fmt.Sscanf(readSum, "%d,%s", &readModTime, &baseSum)
+
+                // If the file has changed, recache it.
+                if readModTime != lastModTime {
+                    baseSum, err = hashAndCache(cachePath, filePath, lastModTime)
+                    if err != nil {
+                        log.Printf("Error hashing and caching: %s - %s", filePath, err)
+                        return
+                    }
                 }
             }
-        }
 
-        sigCheckChan <- uploadJob{
-            filePath: filePath,
-            fileHash: baseSum,
-        }
+            sigCheckChan <- uploadJob{
+                filePath: filePath,
+                fileHash: baseSum,
+            }
+        }(filePath)
     }
+    wg.Wait()
     close(sigCheckChan)
 }
