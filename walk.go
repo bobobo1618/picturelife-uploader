04b1dd91ec5d05ef366f7b65c765b57cc9c25ae0
bobobo1618 <bobobo1618@gmail.com>
2014-05-18 02:21:30 +1000

    Parallelize walking.

--- walk.go
+++ walk.go
@@ -4,21 +4,31 @@ import (
     "path/filepath"
     "os"
     "log"
+    "sync"
 )
 
 func walk(scanPaths []string, hashChan chan <- string) {
+    var waitGroup sync.WaitGroup
+
     for _, filePath := range scanPaths {
-        filepath.Walk(filePath, func(filePath string, info os.FileInfo, err error) error {
-            if !info.IsDir() {
-                absPath, err := filepath.Abs(filePath)
-                if err != nil {
-                    log.Printf("Error getting absolute path for %s - %s\n", filePath, err)
-                    return nil
+        waitGroup.Add(1)
+
+        go func(filePath string){
+            defer waitGroup.Done()
+            filepath.Walk(filePath, func(filePath string, info os.FileInfo, err error) error {
+                if !info.IsDir() {
+                    absPath, err := filepath.Abs(filePath)
+                    if err != nil {
+                        log.Printf("Error getting absolute path for %s - %s\n", filePath, err)
+                        return nil
+                    }
+                    hashChan <- absPath
                 }
-                hashChan <- absPath
-            }
-            return nil
-        })
+                return nil
+            })
+        }(filePath)
     }
+
+    waitGroup.Wait()
     close(hashChan)
 }
